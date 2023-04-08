08896676a373dbd03b2f89727988a113006ed6ed
Pavan R Shetty <126757987+Ucitachi@users.noreply.github.com>
2023-04-09 00:19:52 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -2,24 +2,24 @@
 
 void main()
 {
-    float a,b,ans;
+    float a,b,ans;//Variables declaration
     char op;
 
     printf("Enter the expression:");
-    scanf("%f%c%f\n",&a,&op,&b);
+    scanf("%f%c%f\n",&a,&op,&b);//Expression input
 
     switch(op)
     {
-        case '+':ans=a+b;
+        case '+':ans=a+b;//Sum
                 break;
-        case '-':ans=a-b;
+        case '-':ans=a-b;//Difference
                 break;
-        case '*':ans=a*b;
+        case '*':ans=a*b;//Product
                  break;
-        case '/':ans=a/b;
+        case '/':ans=a/b;//Division
                 break;
         default:printf("Enter a valid operand.");
     }
-    printf("ANS=%.2f.",ans);
+    printf("ANS=%.2f.",ans);//Print answer
     
 }
