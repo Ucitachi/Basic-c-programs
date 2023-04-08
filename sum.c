5ee870114182f1bd02034c13a41057c6ae6b43a7
Pavan R Shetty <126757987+Ucitachi@users.noreply.github.com>
2023-04-09 00:24:30 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,18 +1,20 @@
+//sum of n natural even numbers
 #include <stdio.h>
 
 void main()
 {
     int n,sum=0;
     printf("value of n:");
-    scanf("%d",&n);
+    scanf("%d",&n);//
 
     for(int i=1;i<=n;i++)
-    {
-        if(i%2!=0)
+    { 
+        //Problem statement
+        if(i%2!=0)//If number not even skip it
         {
             continue;
         }
         sum=sum+i;
     }
-    printf("%d",sum);
+    printf("%d",sum);//print answer
 }
